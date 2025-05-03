3fe2bcfde239e750e308f3bfed91713e3722519b
TravelerAs <chengyongliu@foxmail.com>
2025-05-03 16:31:27 +0800

    feat(fix): custom css and add doc

--- lib/album.ts
+++ lib/album.ts
@@ -3,7 +3,8 @@ import { AlbumProps } from "./types";
 export const AlbumArr: AlbumProps[] = [
   {
     link: "https://onoex-img.oss-cn-beijing.aliyuncs.com/images/202410181418947.jpg",
-    description: "This is an image about 'JinXi' from the game Wuthering Waves.",
+    description:
+      "This is an image about 'JinXi' from the game Wuthering Waves.",
     date: "2024-11-26",
   },
   {
@@ -40,5 +41,37 @@ export const AlbumArr: AlbumProps[] = [
     link: "https://onoex-img.oss-cn-beijing.aliyuncs.com/images/202504251733224.jpg",
     description: "Xiamen, Wuyuan Bay Wetland Park, mobile photography.",
     date: "2024-11-02",
+  },
+  {
+    link: "https://onoex-img.oss-cn-beijing.aliyuncs.com/images/202505031529745.jpg",
+    description:
+      "Guizhou, Wolongtan in Libo Xiaoqikong Scenic Area, mobile photography.",
+    date: "2025-05-02",
+  },
+  {
+    link: "https://onoex-img.oss-cn-beijing.aliyuncs.com/images/202505031530271.jpg",
+    description:
+      "Guizhou, Wolongtan in Libo Xiaoqikong Scenic Area, mobile photography.",
+    date: "2025-05-02",
+  },
+  {
+    link: "https://onoex-img.oss-cn-beijing.aliyuncs.com/images/202505031534748.jpg",
+    description:
+      "Guizhou, Layapubu in Libo Xiaoqikong Scenic Area, mobile photography.",
+    date: "2025-05-02",
+  },
+
+  {
+    link: "https://onoex-img.oss-cn-beijing.aliyuncs.com/images/202505031537145.jpg",
+    description:
+      "Guizhou, Xiaoqikong in Libo Xiaoqikong Scenic Area, mobile photography.",
+    date: "2025-05-02",
+  },
+
+  {
+    link: "https://onoex-img.oss-cn-beijing.aliyuncs.com/images/202505031538722.jpg",
+    description:
+      "Guizhou, Xiaoqikong in Libo Xiaoqikong Scenic Area, mobile photography.",
+    date: "2025-05-02",
   },
 ];
