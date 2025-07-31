190b8781dc285411476c006d43cf40d21d3d057a
Liminal Aura Collective <chengyongliu@foxmail.com>
2025-07-31 20:56:26 +0800

    Add comments

--- app/speak-mind/page.tsx
+++ app/speak-mind/page.tsx
@@ -1,5 +1,9 @@
+import Comments from "@/components/ui/comments";
 import MindsList from "@/components/ui/minds-list";
 
 export default function SpeakMind() {
-  return <MindsList />;
+  return <>
+  <MindsList />
+  <Comments term="/minds-block"/>
+  </>;
 }
